0b4152fb8ad9c517e19993a50d60cca83aeb1563
Ethan McGimpsey <ethanmcgimpsey@gmail.com>
2019-10-23 15:42:19 +1100

    Update #18

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public menuSelect MenuSelect = menuSelect.Play;
+    public GameObject SettingsGO;
+
+    private void Update()
+    {
+        if (Settings.SettingsOpen)
+        {
+            SettingsGO.SetActive(true);
+        }
+        else if (!Settings.SettingsOpen)
+        {
+            SettingsGO.SetActive(false);
+        }
+    }
+
+    public void ClickButton()
+    {
+        switch (MenuSelect)
+        {
+            case menuSelect.Play:
+                break;
+            case menuSelect.Settings:
+                Settings.SettingsOpen = !Settings.SettingsOpen;
+                break;
+            case menuSelect.Quit:
+                Application.Quit();
+                break;
+        }
+    }
+
+    public enum menuSelect
+    {
+        Play,
+        Settings,
+        Quit
+    }
+}
